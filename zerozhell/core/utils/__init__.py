932bbc2d61871161f6c88f397275e3a9b491e16d
hexzehn3x7 <hexzehn3x7@blackzspace.de>
2025-04-07 12:24:59 +0200

    Added

--- zerozhell/core/utils/__init__.py
+++
@@ -1,5 +0,0 @@
-from .tools import *
-from .banner import logo
-from .startup import check_dependencies
-from .about import about
-from .update import update
--- zerozhell/core/utils/version.py
+++
@@ -1,3 +0,0 @@
-from __future__ import print_function
-
-version = "0.0.1"
